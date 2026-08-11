3b818a68cb23d20765152cfed09559ba2caada83
Your Name <you@example.com>
2026-08-11 18:21:01 +0530

    final change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Add JWT token to every request
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
@@ -15,7 +17,9 @@ api.interceptors.request.use(
 
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
+  }
 );
 
 export default api;
--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,9 +1,9 @@
 import api from "./api";
 
-export const loginUser = async (data) => {
-  return await api.post("/auth/login", data);
+export const loginUser = (data) => {
+  return api.post("/auth/login", data);
 };
 
-export const registerUser = async (data) => {
-  return await api.post("/auth/register", data);
+export const registerUser = (data) => {
+  return api.post("/auth/register", data);
 };
