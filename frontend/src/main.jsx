97489ffab23cd2e60f521a5f2a6bd565901cee52
Your Name <you@example.com>
2026-08-11 17:23:08 +0530

    fixed again

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import { AuthProvider } from "./context/authcontext.jsx";
+import { AuthProvider } from "./context/Authcontext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
